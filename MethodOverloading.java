680e3bcce34f8ab95df68f603a133c96e2795f95
centuraagatha <centuraagatha_bscs@plmun.edu.ph>
2025-02-26 22:57:43 +0800

    Submitting OBJPROG Week 05 - Laboratory # 16

---
+++ MethodOverloading.java
@@ -0,0 +1,27 @@
+public class MethodOverloading {
+    public static void main(String[] args) {
+
+    public class MethodOverloading {
+    public static void printValue(int number) {
+    System.out.println("Integer value: " + number);
+    }
+}
+    public class MethodOverloading {
+    public static void printValue(double number) {
+        System.out.println("Double value: " + number);
+    }
+}
+    public class MethodOverloading {
+    public static void printValue(String text) {
+        System.out.println("String value: " + text);
+    }
+}
+    public class MethodOverloading {
+    public static void main(String args) {
+        printValue(10);
+        printValue(3.14);
+        printValue("Hello!");
+    }
+}
+    }
+}
